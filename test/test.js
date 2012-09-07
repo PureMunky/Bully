9c76a20800ae4ed70364ac3224cc38d103d8a2ee
Phil Corbett <corbett.phil@gmail.com>
2012-09-06 21:23:35 -0400

    Update send test.

--- test/test.js
+++ test/test.js
@@ -93,3 +93,10 @@ test('Send Exists', function(){
 	expect(1);
 	ok(Bully.Send, 'Send function exists.');
 });
+
+test('Send Message - No Init', function() {
+    Bully.Send('Send Message - No Init', 'ms4Kc19KyK63AnIklfqr5j8PUqPugM', 'Hiegxfia0UWmlNmtqpbUab8UNtr1Nb');
+    expect(1);
+    ok('Did Message Send?');
+});
+
